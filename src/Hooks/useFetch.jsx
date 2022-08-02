1930f477131a1c2985568e48f1d7d73001f8772a
nina romay dev <nina.romay@gmail.com>
2022-08-02 10:03:45 +0200

    Update useFetch.jsx

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -3,16 +3,19 @@ import { useEffect, useState } from "react";
 export const useFetch = () => {
     const [fetchData, setFetchData] = useState({  loading : false })
 
+    let url = process.env.heroku || 'http://localhost:3002/'
+
     useEffect( () => {
         let datos
         
-        fetch('http://localhost:3002/')
+        fetch( url )
         .then( res => res.json())
         .then( (dat) => datos = dat )
         .catch( err   => setFetchData({...fetchData, error: err}) )
         .finally( (dat) => {
             setFetchData({...datos, loading : true})
         })
+        
     }, []);
 
     
