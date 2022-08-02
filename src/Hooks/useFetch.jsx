ea7827b4ef7d96f875d2833001e52abb300b7331
nina romay dev <nina.romay@gmail.com>
2022-08-02 10:25:52 +0200

    Update useFetch.jsx

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -3,12 +3,8 @@ import { useEffect, useState } from "react";
 export const useFetch = () => {
     const [fetchData, setFetchData] = useState({  loading : false })
 
-    let url2 = import.meta.env.HEROKU || 'http://localhost:3002/'
-    console.log( import.meta.env.VITE_HEROKU )
-    console.log( import.meta )
-
-    let url = 'https://fluffy-triage-api.herokuapp.com/'
-
+    let url = import.meta.env.VITE_HEROKU || 'http://localhost:3002/'
+   
     useEffect( () => {
         let datos
         
